362761a184a6a9d38e339b0833063d615ba183df
Raymond Kwan <rayk199492@gmail.com>
2018-11-17 15:51:06 -0500

    Fake data

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = 9000;
 // IN MEM OBJs
 const PENDING_USERS = {
-  '0243cf5e-98d3-4e17-1234-151e8b7ef750': { 'severity': 55 }
+  '0243cf5e-98d3-4e17-1234-151e8b7ef750': { age: 5, name: 'Raymond', 'severity': 55 },
+  '0243cf5e-98d3-4e17-1234-151e8b7e2323': { age: 8, name: 'Donny', 'severity': 100 },
+  '0243cf5e-98d3-4e17-1234-151e8b7e2342': { age: 10, name: 'Alfred', 'severity': 20 },
+  '0243cf5e-98d3-4e17-1234-151e8b7e2342': { age: 10, name: 'Bob', 'severity': 60 },
+  '0243cf5e-98d3-4e17-1234-151e8s7e2342': { age: 6, name: 'Food', 'severity': 33 },
+  '0243cf5e-98d3-4e17-1234-dd1e8b7e2342': { age: 7, name: 'Fish', 'severity': 28 },
 }
 
 // Middleware
