c406ab55d233d865f75d97f2678b33fc99c6f946
donny <donny1103@hotmail.com>
2018-12-14 14:08:33 -0500

    update features: synchronous queue to other counsellors when one engage a queue

--- ws-server.js
+++ ws-server.js
@@ -13,8 +13,12 @@ PENDING_USERS = {
     '0243cf5e-98d3-4e17-1234-151esd7e2342': { age: 10, name: 'Bob', gender: 'male', 'severity': 60, time: new Date(), sadValue: 3, scaredValue: 3, favoriteColor: 'pink' , careAbout:['brother','dad','mom']},
     '0243cf5e-98d3-4e17-1234-151e8s7e2342': { age: 6, name: 'Food', gender: 'female', 'severity': 33, time: new Date(), sadValue: 2, scaredValue: 7, favoriteColor: 'red' , careAbout:['dad','mom']},
     '0243cf5e-98d3-4e17-1234-dd1e8b7e2342': { age: 7, name: 'Fish', gender: 'female', 'severity': 28, time: new Date(), sadValue: 6, scaredValue: 6, favoriteColor: 'purple' , careAbout:['sister','brother','dad','mom']},
-  }
+  },
+  counsellorId:''
 }
+
+// ENGAGED_USERS
+ENGAGED_USERS = {};
 
 // Set the port to 3001
 const PORT = 3001;
@@ -29,16 +33,34 @@ const server = express()
 const wss = new SocketServer({ server });
 
 // Set broadcast function;
-wss.broadcast = (data, id) => {
+wss.broadcast = (data, id, action) => {
   wss.clients.forEach(client => {
-    if (client.readyState === WebSocket.OPEN && !id) {
-      client.send(data);
-    }
-    if (id){
-      console.log('yeah!!!');
+    if (client.readyState === WebSocket.OPEN) {
+
+      if (!id && !action ){
+        client.send(data);
+      } 
+
+      if (client.type === id) {
+        // Broadcase to client type: counsellor/users
+        client.send(data);
+        console.log(`Send data to ${client.type}`);
+      }
+
+      if (client.id === id && !action) {
+        // Send data to specific user, mainly for sending message
+        client.send(data);
+        console.log(`Send data to ${client.id}`);
+      } 
+      
+      // Broadcast to all but not this specific one
+      if (action === 'excluded' && id !== client.id) {
+        client.send(data);
+        console.log('Excluded broadcase to', client.id)
+      }
     }
   })
-}
+};
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
@@ -53,22 +75,21 @@ wss.on('connection', (ws) => {
   };
 
   ws.id = userId.id;
-
   ws.send(JSON.stringify(userId));
+  ws.send(JSON.stringify(PENDING_USERS));
+
   // Broadcast user count
   const queueCount = {
     type: 'updateCount',
     count: Object.keys(PENDING_USERS.queue).length,
   };
 
-  wss.broadcast(JSON.stringify(queueCount));
-  wss.broadcast(JSON.stringify(PENDING_USERS));
-
   // Handling incoming messages.
   ws.on('message', data => {
     const message = JSON.parse(data);
     switch (message.type) {
       case 'user':
+        ws.type = message.type;
         queueCount.count = Object.keys(PENDING_USERS.queue).length;
 
         if (message.sadValue) {
@@ -76,15 +97,27 @@ wss.on('connection', (ws) => {
         } else {
           message.severity = 1;
         }
-        console.log(PENDING_USERS.queue[message.userId] = message);
-        console.log('added/updated user');
+        console.log('added/updated user',PENDING_USERS.queue[message.userId] = message);
 
-        wss.broadcast(JSON.stringify(PENDING_USERS));
-        wss.broadcast(JSON.stringify(queueCount));
+        wss.broadcast(JSON.stringify(queueCount),'user');
+        wss.broadcast(JSON.stringify(PENDING_USERS),'counsellor');
         break;
+      
+      // Counsellor connected
+      case 'counsellor':
+        ws.type = message.type;
+        break;
+
       case 'startChat':
-        console.log(`Start Chat ${message.counsellorName} with ${message.id}`);
-        wss.broadcast(JSON.stringify(message));
+        let counsellorId = message.counsellor.id;
+        let userId = message.id;
+        console.log(`Start Chat ${message.counsellor.name} with ${userId}`);
+
+        ENGAGED_USERS[userId] = {...PENDING_USERS.queue[userId], counsellorId}
+        delete PENDING_USERS.queue[userId];
+        console.log(ENGAGED_USERS);
+
+        wss.broadcast(JSON.stringify(PENDING_USERS), 'counsellor');
         break;
       case 'toUserMsg':
         console.log('receiving msg');
@@ -97,10 +130,21 @@ wss.on('connection', (ws) => {
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => {
     console.log('Client disconnected');
-    delete PENDING_USERS.queue[userId.id]
-    wss.broadcast(JSON.stringify(PENDING_USERS));
-    // Broadcast new user count
-    queueCount.count = Object.keys(PENDING_USERS.queue).length;
-    wss.broadcast(JSON.stringify(queueCount));
+
+    // 
+    if (ws.type === 'user'){
+      queueCount.count = Object.keys(PENDING_USERS.queue).length;
+      Object.keys(PENDING_USERS.queue).includes(userId.id) ? 
+      delete PENDING_USERS.queue[userId.id] :
+      delete ENGAGED_USERS[userId.id];
+
+      // Disconnect user
+      wss.broadcast(JSON.stringify(PENDING_USERS),'counsellor');
+
+      // Broadcast new user count
+      wss.broadcast(JSON.stringify(queueCount),'user');
+    }
+
+    
   });
 });
