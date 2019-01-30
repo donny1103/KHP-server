e012dd5effdea527efda3fe46e4eaed1c52e036d
donny <donny1103@hotmail.com>
2019-01-30 17:36:23 -0500

    updated

--- server.js
+++ server.js
@@ -1,50 +1,181 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const WebSocket = require('ws');
+const SocketServer = WebSocket.Server;
 const uuidv4 = require('uuid/v4');
-const morgan = require('morgan')
-const app = express();
+const dotenv = require('dotenv').config();
+// Dummy Data
+PENDING_USERS = {
+  type: 'queue',
+  queue: {
+    // '0243cf5e-98d3-4e17-1234-151e8b7ef750': { age: 5, name: 'Raymond', gender: 'male', 'severity': 55, time: new Date(), sadValue: 6, scaredValue: 3, favoriteColor: 'green', careAbout:['friend','teacher','pet','other']},
+    // '0243cf5e-98d3-4e17-1234-151e8b7e2323': { age: 8, name: 'Donny', gender: 'male', 'severity': 100, time: new Date(), sadValue: 3, scaredValue: 3, favoriteColor: 'red', careAbout:['friend','sister','brother','dad','mom']},
+    // '0243cf5e-98d3-4e17-1234-151e8b7e2342': { age: 10, name: 'Alfred', gender: 'male', 'severity': 20, time: new Date(), sadValue: 6, scaredValue: 7, favoriteColor: 'blue' , careAbout:['friend','sister']},
+    // '0243cf5e-98d3-4e17-1234-151esd7e2342': { age: 10, name: 'Jacob', gender: 'male', 'severity': 60, time: new Date(), sadValue: 3, scaredValue: 3, favoriteColor: 'pink' , careAbout:['brother','dad','mom']},
+    // '0243cf5e-98d3-4e17-1234-151e8s7e2342': { age: 6, name: 'Kani', gender: 'female', 'severity': 33, time: new Date(), sadValue: 2, scaredValue: 7, favoriteColor: 'red' , careAbout:['dad','mom']},
+    // '0243cf5e-98d3-4e17-1234-dd1e8b7e2342': { age: 7, name: 'Niveditha', gender: 'female', 'severity': 28, time: new Date(), sadValue: 6, scaredValue: 6, favoriteColor: 'purple' , careAbout:['sister','brother','dad','mom']},
+  },
+  counsellorId:''
+};
 
-const PORT = 9000;
-// IN MEM OBJs
-const PENDING_USERS = {
-  '0243cf5e-98d3-4e17-1234-151e8b7ef750': { age: 5, name: 'Raymond', 'severity': 55, time: new Date() },
-  '0243cf5e-98d3-4e17-1234-151e8b7e2323': { age: 8, name: 'Donny', 'severity': 100, time: new Date() },
-  '0243cf5e-98d3-4e17-1234-151e8b7e2342': { age: 10, name: 'Alfred', 'severity': 20, time: new Date() },
-  '0243cf5e-98d3-4e17-1234-151esd7e2342': { age: 10, name: 'Bob', 'severity': 60, time: new Date() },
-  '0243cf5e-98d3-4e17-1234-151e8s7e2342': { age: 6, name: 'Food', 'severity': 33, time: new Date() },
-  '0243cf5e-98d3-4e17-1234-dd1e8b7e2342': { age: 7, name: 'Fish', 'severity': 28, time: new Date() },
-}
+// ENGAGED_USERS
+ENGAGED_USERS = {};
 
-// Middleware
-app.use(cors());
-app.use(morgan('dev'));
-app.use(bodyParser.json());
+// Set the port to 3001
+const PORT = process.env.PORT || 3001;
 
-// GET routes
-app.get('/userid', (req, res) => {
-  data = {userId: uuidv4()};
-  res.json(data);
-})
+// Create a new express server
+const server = express()
+  // Make the express server serve static assets (html, javascript, css) from the /public folder
+  .use(express.static('public'))
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${PORT}`));
 
-app.get('/pendingusers', (req, res) => {
-  res.json(PENDING_USERS);
-})
+// Create the WebSockets server
+const wss = new SocketServer({ server });
 
-// POST routes
-app.post('/user', (req, res) => {
-  console.log(req.body);
-  user = req.body;
-  PENDING_USERS[user.userId] = user
-  res.send()
+// Set broadcast function;
+wss.broadcast = (data, id, action) => {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+
+      if (!id && !action ){
+        client.send(data);
+      } 
+
+      if (client.type === id) {
+        // Broadcase to client type: counsellor/users
+        client.send(data);
+      }
+
+      if (client.id === id && !action) {
+        // Send data to specific user, mainly for sending message
+        client.send(data);
+      } 
+      
+      // Broadcast to all but not this specific one
+      if (action === 'excluded' && id !== client.id) {
+        client.send(data);
+        console.log('Broadcase exclude:', id)
+      }
+    }
+  })
+};
+
+// Set up a callback that will run when a client connects to the server
+// When a client connects they are assigned a socket, represented by
+// the ws parameter in the callback.
+wss.on('connection', (ws) => {
+  console.log('Client connected. Total', wss.clients.size);
+  // New user detected
+  // Assign ID
+  const userId = {
+    type: 'id',
+    id: uuidv4(),
+    cid: uuidv4(),
+  };
+
+  // Assign id as a key-value pair to the socket object
+  ws.id = userId.id;
+  ws.send(JSON.stringify(userId));
+
+  // Broadcast user count
+  const queueCount = {
+    type: 'updateCount',
+    count: Object.keys(PENDING_USERS.queue).length,
+  };
+
+  // Handling incoming messages.
+  ws.on('message', data => {
+    const message = JSON.parse(data);
+    switch (message.type) {
+      case 'user':
+        ws.type = message.type;
+        queueCount.count = Object.keys(PENDING_USERS.queue).length;
+
+        if (message.sadValue) {
+          message.severity = Math.round(((message.sadValue + message.scaredValue) / 14) * 100);
+        } else {
+          message.severity = 1;
+        }
+        //console.log('added/updated user',PENDING_USERS.queue[message.userId] = message);
+
+        wss.broadcast(JSON.stringify(queueCount),'user');
+        wss.broadcast(JSON.stringify(PENDING_USERS),'counsellor');
+        break;
+      
+      // Counsellor connected
+      case 'counsellor':
+        ws.type = message.type;
+        ws.send(JSON.stringify(PENDING_USERS));
+        break;
+
+      case 'startChat':
+        console.log(`${message.counsellor.name} start Chat  with ${message.name ? message.name : message.userId}`);
+
+        ENGAGED_USERS[message.userId] = {
+          ...PENDING_USERS.queue[message.userId],
+          userId:message.userId, 
+          counsellorId: message.counsellor.id, 
+          messages:[]
+        };
+        
+        delete PENDING_USERS.queue[message.userId];
+        wss.broadcast(JSON.stringify(PENDING_USERS), message.counsellor.id, 'excluded');
+        wss.broadcast(JSON.stringify({
+          type: 'startChat',id:message.userId,
+          counsellor: {...message.counsellor}
+        }), message.userId);
+        break;
+
+      case 'toUserMsg':
+        if(ENGAGED_USERS[message.userId]){
+          ENGAGED_USERS[message.userId].messages.push({...message});
+          wss.broadcast(JSON.stringify(message), message.userId);
+        }
+        break;
+
+      case 'toCounsellorMsg':
+        if(ENGAGED_USERS[message.userId]){
+          ENGAGED_USERS[message.userId].messages.push({...message});
+          wss.broadcast(JSON.stringify(message), message.counsellorId);
+        }
+        break;
+
+      default:
+        console.log('Message not recognised:', message);
+    }
+  });
+
+  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+  ws.on('close', () => {
+
+    switch(ws.type){
+      case 'user':
+        queueCount.count = Object.keys(PENDING_USERS.queue).length;
+        if (ENGAGED_USERS[userId.id] && ENGAGED_USERS[userId.id].counsellorId){
+          console.log('user disconect to this counsellor');
+          let disconnectMsgToCounsellor = {
+            type: 'disconnect',
+            id: userId.id
+          }
+          wss.broadcast(JSON.stringify(disconnectMsgToCounsellor), ENGAGED_USERS[userId.id].counsellorId);
+        }
+
+        Object.keys(PENDING_USERS.queue).includes(userId.id) ? 
+        delete PENDING_USERS.queue[userId.id] :
+        delete ENGAGED_USERS[userId.id];
+
+        // Disconnect user
+        wss.broadcast(JSON.stringify(PENDING_USERS),'counsellor');
+        
+        // Broadcast new user count
+        wss.broadcast(JSON.stringify(queueCount),'user');
+        console.log('User disconnected');
+        break;
+      case 'counsellor':
+        console.log('Counsellor disconnected');
+        break;
+      default:
+        console.log('Disconnect type not recognised:', ws.type);
+    }
+  });
 });
-
-// DELETE routes
-app.delete('/user', (req, res) => {
-  delete PENDING_USERS[req.body.id]
-  res.status(200).send()
-})
-
-// Server Listens
-app.listen(PORT);
-console.log(`Server listening on port ${PORT}`);
